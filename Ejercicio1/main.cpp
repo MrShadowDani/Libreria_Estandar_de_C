3eb067a8124ed8d3be745320e62888abc351ff8a
MrShadowDani <dgonzsan@myuax.com>
2023-12-19 19:12:18 +0100

    guardar

--- Ejercicio1/main.cpp
+++ Ejercicio1/main.cpp
@@ -1,20 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <iostream>
+#include <cstdlib>
+#include <random>
 
 int main() {
     // Declarar un número
     int num = -10;
 
     // Obtener el valor absoluto utilizando abs()
-    int abs_num = abs(num);
+    int abs_num = std::abs(num);
 
-    printf("El valor absoluto de %d es %d\n", num, abs_num);
+    std::cout << "El valor absoluto de " << num << " es " << abs_num << std::endl;
 
-    // Generar un número aleatorio
-    srand(time(NULL));
-    int random_number = rand();
-    printf("Número aleatorio: %d\n", random_number);
+    // Generar un número aleatorio utilizando la biblioteca de C++11
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dis(1, 100); // Rango de números aleatorios (1 a 100)
+
+    int random_number = dis(gen);
+    std::cout << "Numero aleatorio: " << random_number << std::endl;
 
     return 0;
 }
