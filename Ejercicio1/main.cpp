bc9078882331f31fb4c06aea772fded3c3ac7c8b
MrShadowDani <dgonzsan@myuax.com>
2023-12-19 19:10:27 +0100

    guardar

--- Ejercicio1/main.cpp
+++ Ejercicio1/main.cpp
@@ -1,14 +1,20 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-    // Declarar un número negativo
+    // Declarar un número
     int num = -10;
 
     // Obtener el valor absoluto utilizando abs()
     int abs_num = abs(num);
 
     printf("El valor absoluto de %d es %d\n", num, abs_num);
+
+    // Generar un número aleatorio
+    srand(time(NULL));
+    int random_number = rand();
+    printf("Número aleatorio: %d\n", random_number);
 
     return 0;
 }
